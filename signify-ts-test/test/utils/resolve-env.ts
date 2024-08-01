0b8bb1980108adc42c88ad8face0e30b47a226e5
Lance <lance.byrd@gleif.org>
2024-08-01 09:03:28 -0400

    Improved tests to support multiple TEST_ENVIRONMENT (#28)
    * improved testing, including TEST_ENVIRONMENT options for rootsid/nordlei
    
    Signed-off-by: 2byrds <2byrds@gmail.com>
    
    * changed default TEST_ENV to docker and improved README
    
    Signed-off-by: 2byrds <2byrds@gmail.com>
    
    ---------
    
    Signed-off-by: 2byrds <2byrds@gmail.com>

--- signify-ts-test/test/utils/resolve-env.ts
+++ signify-ts-test/test/utils/resolve-env.ts
@@ -1,4 +1,4 @@
-export type TestEnvironmentPreset = 'local' | 'docker';
+export type TestEnvironmentPreset = 'local' | 'docker' | 'rootsid_dev' | 'rootsid_test' | 'nordlei_dev' | 'nordlei_test';
 
 export interface TestEnvironment {
     preset: TestEnvironmentPreset;
@@ -18,15 +18,12 @@ export function resolveEnvironment(
 ): TestEnvironment {
     const preset = input ?? process.env.TEST_ENVIRONMENT ?? 'docker';
 
-    const url = 'http://127.0.0.1:3901';
-    const bootUrl = 'http://127.0.0.1:3903';
-
     switch (preset) {
         case 'docker':
             return {
                 preset: preset,
-                url,
-                bootUrl,
+                url: 'http://127.0.0.1:3901',
+                bootUrl: 'http://127.0.0.1:3903',
                 witnessUrls: [
                     'http://witness-demo:5642',
                     'http://witness-demo:5643',
@@ -38,8 +35,8 @@ export function resolveEnvironment(
         case 'local':
             return {
                 preset: preset,
-                url,
-                bootUrl,
+                url: 'http://127.0.0.1:3901',
+                bootUrl: 'http://127.0.0.1:3903',
                 vleiServerUrl: 'http://localhost:7723',
                 witnessUrls: [
                     'http://localhost:5642',
@@ -47,6 +44,76 @@ export function resolveEnvironment(
                     'http://localhost:5644',
                 ],
                 witnessIds: [WAN, WIL, WES],
+            };
+        case 'rootsid_dev':
+            return {
+                preset: preset,
+                // url: "http://keria--publi-7wqhypzd56ee-cc3c56cbeced4f45.elb.us-east-1.amazonaws.com/admin",
+                // bootUrl: "http://keria--publi-7wqhypzd56ee-cc3c56cbeced4f45.elb.us-east-1.amazonaws.com:3903",
+                url: "https://keria-dev.rootsid.cloud/admin",
+                bootUrl: "https://keria-dev.rootsid.cloud",
+                witnessUrls: [
+                    "https://witness-dev01.rootsid.cloud", 
+                    "https://witness-dev02.rootsid.cloud",
+                    "https://witness-dev03.rootsid.cloud"
+                ],
+                witnessIds: [WAN, WIL, WES],
+                vleiServerUrl: 'http://schemas.rootsid.cloud',
+            };
+        case 'rootsid_test':
+            return {
+                preset: preset,
+                url: "https://keria-demoservice.rootsid.cloud/admin",
+                bootUrl: "https://keria-demoservice.rootsid.cloud",
+                witnessUrls: [
+                    "https://witness-dev01.rootsid.cloud", 
+                    "https://witness-dev02.rootsid.cloud",
+                    "https://witness-dev03.rootsid.cloud"
+                ],
+                witnessIds: [WAN, WIL, WES],
+                vleiServerUrl: 'http://schemas.rootsid.cloud',
+            };
+        case 'nordlei_dev':
+            return {
+                preset: preset,
+                url: "https://demo.wallet.vlei.tech/",
+                bootUrl: "https://demo.wallet.vlei.tech/boot",
+                witnessUrls: [
+                    "https://william.witness.vlei.io/oobi",
+                    "https://wesley.witness.vlei.io/oobi",
+                    "https://whitney.witness.vlei.io/oobi",
+                    "https://wilma.witness.vlei.io/oobi",
+                    "https://wilbur.witness.vlei.io/oobi"
+                ],
+                witnessIds: [
+                    "BB6_wAm4rtFPRFg1qJHbC1RWNcRKMth2sFw6MgSqFKg_",
+                    "BGJvFwob-UV5J1vSbuCroz27k4FGaZE992K4sc79cD54",
+                    "BMMOAZ4Ujv0jP3VhCAHmx9yTSBoP1sAoDjFXas14JYG-",
+                    "BIrxc3loHN4kQ2HN8Ev-bisMBZzkdfXQdwl4KKdy2iZh",
+                    "BDTChgVW3pAxkYCYDVWV9DQYu_FTZ8laD-WhpFHvY9SQ"
+                ],
+                vleiServerUrl: 'http://schemas.rootsid.cloud',
+            };
+        case 'nordlei_test':
+            return {
+                preset: preset,
+                url: "https://demo.wallet.vlei.tech/",
+                bootUrl: "https://demo.wallet.vlei.tech/boot",
+                witnessUrls: [
+                    "https://william.witness.vlei.io/oobi",
+                    "https://wesley.witness.vlei.io/oobi",
+                    "https://whitney.witness.vlei.io/oobi",
+                    "https://wilma.witness.vlei.io/oobi",
+                    "https://wilbur.witness.vlei.io/oobi"
+                ],
+                witnessIds: [
+                    "BB6_wAm4rtFPRFg1qJHbC1RWNcRKMth2sFw6MgSqFKg_",
+                    "BGJvFwob-UV5J1vSbuCroz27k4FGaZE992K4sc79cD54",
+                    "BMMOAZ4Ujv0jP3VhCAHmx9yTSBoP1sAoDjFXas14JYG-",
+                    "BIrxc3loHN4kQ2HN8Ev-bisMBZzkdfXQdwl4KKdy2iZh",
+                    "BDTChgVW3pAxkYCYDVWV9DQYu_FTZ8laD-WhpFHvY9SQ"
+                ],
+                vleiServerUrl: 'http://schemas.rootsid.cloud',
             };
         default:
             throw new Error(`Unknown test environment preset '${preset}'`);
