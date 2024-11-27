59fd1e67bf6e332a342e5b23f11a3753006229dc
Charles Lanahan <charles.lanahan@gmail.com>
2024-11-27 15:52:01 -0500

    Update jest.config.ts to remove warning.
    jest.config.ts throws a warning when 'watch: false` is set.  Removed to get rid of the warning.

--- signify-ts-test/test/jest.config.ts
+++ signify-ts-test/test/jest.config.ts
@@ -2,8 +2,7 @@ import { Config } from "jest";
 
 const config: Config = {
   preset: "ts-jest",
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
-  watch: false,
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"]
 };
 
 export default config;
