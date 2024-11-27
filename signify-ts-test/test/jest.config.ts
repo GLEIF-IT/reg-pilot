bc21dd59ae8e93f7e47d1127607d4baaf2719d69
Charles Lanahan <charles.lanahan@gmail.com>
2024-11-27 15:57:34 -0500

    Update jest.config.ts
    Added stupid comma back.  I hope this works.

--- signify-ts-test/test/jest.config.ts
+++ signify-ts-test/test/jest.config.ts
@@ -2,7 +2,7 @@ import { Config } from "jest";
 
 const config: Config = {
   preset: "ts-jest",
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"]
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
 };
 
 export default config;
