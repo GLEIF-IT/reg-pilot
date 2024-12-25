d78d346d415c4fa4b37982a223b708f5bd77ef74
Aidar Negimatzhanov <aydar.negimatzhanov@perfectart.com>
2024-12-25 21:53:48 +0100

    Fix add root of trust for singlesig

--- signify-ts-test/src/api-adapter.ts
+++ signify-ts-test/src/api-adapter.ts
@@ -92,7 +92,25 @@ export class ApiAdapter {
     return resp;
   }
 
-  public async addRootOfTrust(configJson: any): Promise<Response> {
+  public hasGLEIFWithMultisig(data: any): boolean {
+    return data.users.some(
+      (user: any) =>
+        user.type === "GLEIF" &&
+        user.identifiers.some((id: any) => data.identifiers[id]?.identifiers)
+    );
+  }
+
+  public async addRootOfTrust(configJson: any): Promise<Response> {    
+    if(this.hasGLEIFWithMultisig(configJson)){
+      return await this.addRootOfTrustMultisig(configJson);
+    }
+    else{
+      return await this.addRootOfTrustSinglesig(configJson);
+    }
+  }
+
+
+  public async addRootOfTrustMultisig(configJson: any): Promise<Response> {
     const rootOfTrustMultisigIdentifierName = configJson.users
       .filter(
         (usr: any) => usr.type == "GLEIF" || usr.type == "GLEIF_EXTERNAL",
@@ -141,6 +159,49 @@ export class ApiAdapter {
       vlei: oobiRespBody,
       aid: rootOfTrustAid.prefix,
       oobi: oobiUrl,
+    };
+    let lreq = {
+      headers: heads,
+      method: "POST",
+      body: JSON.stringify(lbody),
+    };
+    const lurl = `${this.apiBaseUrl}/add_root_of_trust`;
+    const lresp = await fetch(lurl, lreq);
+    return lresp;
+  }
+
+  public async addRootOfTrustSinglesig(configJson: any): Promise<Response> {  
+    const rootOfTrustIdentifierName = configJson.users.filter(
+      (usr: any) => usr.type == "GLEIF",
+    )[0].identifiers[0];
+    const rootOfTrustIdentifierAgent =
+      configJson.agents[
+        configJson.identifiers[rootOfTrustIdentifierName].agent
+      ];
+    const rootOfTrustIdentifierSecret =
+      configJson.secrets[rootOfTrustIdentifierAgent.secret];
+    const clients = await getOrCreateClients(
+      1,
+      [rootOfTrustIdentifierSecret],
+      true,
+    );
+    const client = clients[clients.length - 1];
+    const rootOfTrustAid = await client
+      .identifiers()
+      .get(rootOfTrustIdentifierName);
+
+    const oobi = await client.oobis().get(rootOfTrustIdentifierName);
+    let oobiUrl = oobi.oobis[0];
+    const url = new URL(oobiUrl);
+    if (url.hostname === "keria")
+      oobiUrl = oobiUrl.replace("keria", "localhost");
+    const oobiResp = await fetch(oobiUrl);
+    const oobiRespBody = await oobiResp.text();
+    const heads = new Headers();
+    heads.set("Content-Type", "application/json");
+    let lbody = {
+      vlei: oobiRespBody,
+      aid: rootOfTrustAid.prefix,
     };
     let lreq = {
       headers: heads,
