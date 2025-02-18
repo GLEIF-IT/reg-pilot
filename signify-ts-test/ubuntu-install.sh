1b46d50d6405e32d6da4df58b1c556e74ff5bb87
Lance <lance.byrd@rootsid.com>
2025-02-18 13:56:47 -0500

    use sudo with npm test to launch docker
    Signed-off-by: Lance <lance.byrd@rootsid.com>

--- signify-ts-test/ubuntu-install.sh
+++ signify-ts-test/ubuntu-install.sh
@@ -28,8 +28,8 @@ echo   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docke
   $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" |   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-npm test
 sudo docker ps
+
 sudo npm test
 
 
